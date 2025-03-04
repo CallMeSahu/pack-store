21993c0db1286ea541dc42988f274eae1442ed3f
Siddhartha Sahu <siddharthasahu.work@gmail.com>
2025-03-05 04:46:40 +0530

    routes config

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const db = require("./config/mongoose-connection");
+const ownerRouter = require("./routes/ownerRouter");
+const userRouter = require("./routes/userRouter");
+const productRouter = require("./routes/productRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+
+app.use("/owner", ownerRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.listen(3000);
